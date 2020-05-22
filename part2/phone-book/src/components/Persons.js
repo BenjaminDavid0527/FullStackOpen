6a92c5525a24287af7adeb02019830b8139727db
Ben <ben.price2013@gmail.com>
2020-05-22 17:08:09 -0500

    missed adding app and persons files

--- part2/phone-book/src/components/Persons.js
+++ part2/phone-book/src/components/Persons.js
@@ -3,10 +3,12 @@ import Person from './Person'
 
 const Persons = (props) => {
     let persons = props.persons
+    
     if (props.filter !== '') {
         let block = props.filter.toLowerCase()
-        persons = persons
-        .filter((person) => person.name.toLowerCase().includes(block))
+        persons =  persons.filter((person) => 
+                person.name.toLowerCase().includes(block)
+                )
     }
     return (
     persons.map(person => 
